ad340e2fa709a1a77be8d4b32605eac2740d06cf
rodolfostark <rodolfojonatas@outlook.com>
2024-05-11 23:02:01 -0300

    chore: add validation layer for expenses request fields

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,7 +7,26 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        // TO DO VALIDATIONS 
+        Validate(request);
         return new ResponseRegisteredExpenseJson();
+    }
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Title))
+        {
+            throw new ArgumentException("The title is required");
+        }
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero");
+        }
+        if (DateTime.Compare(request.Date, DateTime.UtcNow) > 0)
+        {
+            throw new ArgumentException("Expenses cannot be from the future");
+        }
+        if (!Enum.IsDefined<PaymentType>(request.PaymentType))
+        {
+            throw new ArgumentException($"Invalid payment type {request.PaymentType}");
+        }
     }
 }
