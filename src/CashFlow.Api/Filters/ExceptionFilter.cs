77e811216f9a24c1d52d688c7a60f6603c180631
rodolfostark <rodolfojonatas@outlook.com>
2024-05-13 22:28:00 -0300

    chore: setup exception filter

---
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnknownError(context);
+        }
+    }
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException)
+        {
+            var exception = (ErrorOnValidationException) context.Exception;
+            var errorResponse = new ResponseErrorJson(exception.Errors);
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new ObjectResult(errorResponse);
+        }
+    }
+    private void ThrowUnknownError(ExceptionContext context)
+    {
+        var errorResponse = new ResponseErrorJson("Unknown Error");
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
