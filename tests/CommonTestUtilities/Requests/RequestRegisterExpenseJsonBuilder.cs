7a683b196dd905a1bdb90f5df8f6795587c03c6f
rodolfostark <rodolfojonatas@outlook.com>
2024-05-16 23:01:32 -0300

    chore: setup fluent syntax to the arrange tests with bogus

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        var faker = new Faker<RequestRegisterExpenseJson>();
+        var request = faker
+            .RuleFor(rule => rule.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(rule => rule.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(rule => rule.Date, faker => faker.Date.Past())
+            .RuleFor(rule => rule.Amount, faker => faker.Random.Decimal(min: 1, max: 1000))
+            .RuleFor(rule => rule.PaymentType, faker => faker.PickRandom<PaymentType>());
+        return request;
+    }
+}
