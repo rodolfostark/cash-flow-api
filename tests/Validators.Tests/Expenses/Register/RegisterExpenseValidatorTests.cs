9f318843a83751d84054bd2a7963868b5911ce88
rodolfostark <rodolfojonatas@outlook.com>
2024-05-19 15:11:43 -0300

    test: add test for error title empty scenario

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
@@ -17,5 +18,19 @@ public class RegisterExpenseValidatorTests
 
         // Assert
         result.IsValid.Should().BeTrue();
+    }
+    [Fact]
+    public void ErrorTitleEmpty() { 
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED_ERROR));
     }
 }
