4eae58a8375d5a943ab5bf62e0f1e2895c2f4b2d
rodolfostark <rodolfojonatas@outlook.com>
2024-05-23 18:19:32 -0300

    chore: setup dbcontext and the use case

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,6 +1,8 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
 using CashFlow.Exception.ExceptionsBase;
+using CashFlow.Infrastructure.DataAccess;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 public class RegisterExpenseUseCase
@@ -8,6 +10,21 @@ public class RegisterExpenseUseCase
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
+
+        var dbContext = new CashFlowDbContext();
+
+        var expense = new Expense
+        {
+            Amount = request.Amount,
+            Date = request.Date,
+            Description = request.Description,
+            Title = request.Title,
+            PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
+        };
+
+        dbContext.Expenses.Add(expense);
+        dbContext.SaveChanges();
+
         return new ResponseRegisteredExpenseJson();
     }
     private void Validate(RequestRegisterExpenseJson request)
