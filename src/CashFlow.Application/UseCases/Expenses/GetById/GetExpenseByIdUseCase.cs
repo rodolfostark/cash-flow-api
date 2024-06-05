fd250abfa738a94285265971674dcbe0980a696c
rodolfostark <rodolfojonatas@outlook.com>
2024-06-04 22:11:52 -0300

    chore: add error message for expense not found error

--- src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
 
 namespace CashFlow.Application.UseCases.Expenses.GetById;
 public class GetExpenseByIdUseCase(IExpensesRepository expensesRepository, IMapper mapper) : IGetExpenseByIdUseCase
@@ -11,6 +12,11 @@ public class GetExpenseByIdUseCase(IExpensesRepository expensesRepository, IMapp
     public async Task<ResponseExpenseJson> Execute(long id)
     {
         var expense = await _expensesRepository.GetById(id);
+        if (expense is null)
+        {
+            throw new DirectoryNotFoundException(ResourceErrorMessages.);
+        }
+        
         var response = _mapper.Map<ResponseExpenseJson>(expense);
         return response;
     }
