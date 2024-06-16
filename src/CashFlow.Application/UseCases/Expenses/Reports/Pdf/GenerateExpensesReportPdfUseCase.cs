f98b2b2c1921345bc1b9cceaeedd8037ef8f5dc9
rodolfostark <rodolfojonatas@outlook.com>
2024-06-16 17:29:25 -0300

    chore: add generation report pdf boilerplate

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,17 @@
+﻿
+using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+public class GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository expensesReadOnlyRepository) : IGenerateExpensesReportPdfUseCase
+{
+    private readonly IExpensesReadOnlyRepository _expensesReadOnlyRepository = expensesReadOnlyRepository;
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expenses = await _expensesReadOnlyRepository.GetExpensesByMonth(month);
+        if (expenses.Count == 0)
+        {
+            return [];
+        }
+        return [];
+    }
+}
