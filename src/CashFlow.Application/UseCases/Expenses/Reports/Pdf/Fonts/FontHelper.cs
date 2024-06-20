d7ab3bbb827900217e8f7010706f323e83327f49
rodolfostark <rodolfojonatas@outlook.com>
2024-06-19 22:04:25 -0300

    chore: create resolver and helper to manage fonts

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
@@ -0,0 +1,5 @@
+﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+public class FontHelper
+{
+    public const string RALEWAY_REGULAR = "Raleway-Regular";
+}
--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,10 +1,16 @@
-﻿
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Repositories.Expenses;
+using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
-public class GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository expensesReadOnlyRepository) : IGenerateExpensesReportPdfUseCase
+public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
 {
-    private readonly IExpensesReadOnlyRepository _expensesReadOnlyRepository = expensesReadOnlyRepository;
+    private readonly IExpensesReadOnlyRepository _expensesReadOnlyRepository;
+    public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository expensesReadOnlyRepository)
+    {
+        _expensesReadOnlyRepository = expensesReadOnlyRepository;
+        GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
+    }
     public async Task<byte[]> Execute(DateOnly month)
     {
         var expenses = await _expensesReadOnlyRepository.GetExpensesByMonth(month);
