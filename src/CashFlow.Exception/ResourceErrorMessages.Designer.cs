731b3eb7383825514cd0d5a13226d823a225800f
rodolfostark <rodolfojonatas@outlook.com>
2024-06-29 21:30:16 -0300

    :wrench: feat: add new resource error messages

--- src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
+++ src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
@@ -70,6 +70,15 @@ namespace CashFlow.Exception {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The email cannot be empty.
+        /// </summary>
+        public static string EMAIL_EMPTY {
+            get {
+                return ResourceManager.GetString("EMAIL_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a Expense not found.
         /// </summary>
         public static string EXPENSE_NOT_FOUND {
@@ -84,6 +93,24 @@ namespace CashFlow.Exception {
         public static string EXPENSES_CANNOT_FROM_THE_FUTURE {
             get {
                 return ResourceManager.GetString("EXPENSES_CANNOT_FROM_THE_FUTURE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Your password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one number.
+        /// </summary>
+        public static string INVALID_PASSWORD {
+            get {
+                return ResourceManager.GetString("INVALID_PASSWORD", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The name cannot be empty.
+        /// </summary>
+        public static string NAME_EMPTY {
+            get {
+                return ResourceManager.GetString("NAME_EMPTY", resourceCulture);
             }
         }
         
