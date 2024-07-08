d313b85a3b83ae0305350e51e16c2d4c363ad1a3
rodolfostark <rodolfojonatas@outlook.com>
2024-07-07 23:39:44 -0300

    :recycle: refactor: add async and as no tracking to get user by email

--- src/CashFlow.Infrastructure/DataAccess/Repositories/UserRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -9,16 +9,20 @@ internal class UserRepository(CashFlowDbContext dbContext) : IUserReadOnlyReposi
 
     public async Task Add(User user)
     {
-        await _dbContext.Users.AddAsync(user);
+        await _dbContext.Users
+            .AddAsync(user);
     }
 
     public async Task<bool> ExistActiveUserWithEmail(string email)
     {
-        return await _dbContext.Users.AnyAsync(user => user.Email.Equals(email));
+        return await _dbContext.Users
+            .AnyAsync(user => user.Email.Equals(email));
     }
 
-    public Task<User?> GetUserByEmail(string email)
+    public async Task<User?> GetUserByEmail(string email)
     {
-        return _dbContext.Users.FirstOrDefaultAsync(user => user.Email.Equals(email));
+        return await _dbContext.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Email.Equals(email));
     }
 }
