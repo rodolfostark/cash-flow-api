813a70395739bdae6c3852de12d0e6548c633b1d
rodolfostark <rodolfojonatas@outlook.com>
2024-07-13 14:35:36 -0300

    :sparkles: feat: adding unit tests for user register validator

---
+++ tests/Validators.Tests/Users/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,55 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+namespace Validators.Tests.Users.Register;
+public class RegisterUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        // Act
+        var result = validator.Validate(request);
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("     ")]
+    [InlineData(null)]
+    public void Error_Name_Empty(string name)
+    {
+        // Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = name;
+        // Act
+        var result = validator.Validate(request);
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle()
+            .And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.NAME_EMPTY));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("     ")]
+    [InlineData(null)]
+    public void Error_Email_Empty(string email)
+    {
+        // Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Email = email;
+        // Act
+        var result = validator.Validate(request);
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EMAIL_EMPTY));
+    }
+
+}
