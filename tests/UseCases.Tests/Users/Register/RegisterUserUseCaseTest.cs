3933fbac764d405ffd980d904051542ad8493f3c
rodolfostark <rodolfojonatas@outlook.com>
2024-07-14 15:01:21 -0300

    :sparkles: feat: adding new unit tests project for use cases

---
+++ tests/UseCases.Tests/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace UseCases.Tests.Users.Register;
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        var response = await useCase.Execute(request);
+        response.Should().NotBeNull();
+        response.Name.Should().Be(request.Name);
+        response.Token.Should().NotBeNullOrWhiteSpace();
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        return new RegisterUserUseCase();
+    }
+}
