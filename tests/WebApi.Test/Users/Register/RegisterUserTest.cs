847e7e93af64632555a5cd8b8c2ab061171cb6c8
rodolfostark <rodolfojonatas@outlook.com>
2024-08-15 21:09:06 -0300

    :sparkles: feat: setup user register integration test

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using CommonTestUtilities.Requests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
 
 namespace WebApi.Test.Users.Register;
 public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
 {
+    private readonly HttpClient _httpClient;
+    public RegisterUserTest(WebApplicationFactory<Program> webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+    }
     [Fact]
     public async Task Success()
     {
+        var request = RequestRegisterExpenseJsonBuilder.Build();
 
+        _httpClient.PostAsJsonAsync(, request);
     }
 }
